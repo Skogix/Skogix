c2fadedbe0ecdd4dcbc94d8a3ace5e07cf730a0e
skogix <skogix@gmail.com>
2021-09-19 18:24:03 +0200

    basic subroutines

--- Main/Counter.fs
+++ Main/Counter.fs
@@ -4,21 +4,63 @@ module Counter =
     open Avalonia.Controls
     open Avalonia.FuncUI.DSL
     open Avalonia.Layout
+    open Elmish
     
-    type State = { count : int }
-    let init = { count = 0 }
+    
+    type State = { count : int
+                   running: bool }
+    let init() = { count = 0
+                   running = true }, Cmd.none
 
-    type Msg = Increment | Decrement | Reset
+    type Msg =
+      | IncrementIfRunning 
+      | Increment
+      | IncrementDelayed
+      | Decrement
+      | Reset
+      | RunningTrue
+      | RunningFalse
 
-    let update (msg: Msg) (state: State) : State =
+    let update (msg: Msg) (state: State) : State * Cmd<_> =
         match msg with
-        | Increment -> { state with count = state.count + 1 }
-        | Decrement -> { state with count = state.count - 1 }
-        | Reset -> init
+        | Increment -> { state with count = state.count + 1 }, Cmd.none
+        | Decrement -> { state with count = state.count - 1 }, Cmd.none
+        | IncrementIfRunning ->
+          match state.running with
+          | true -> {state with count = state.count + 1}, Cmd.none
+          | false -> state, Cmd.none
+        | Reset ->
+          let initState, cmd = init()
+          initState, cmd
+        | IncrementDelayed ->
+          let incrementDelayedCmd (dispatch: Msg -> unit) : unit =
+            let delayedDispatch = async {
+              do! Async.Sleep 1000
+              dispatch Increment
+            }
+            Async.StartImmediate delayedDispatch
+          state, Cmd.ofSub incrementDelayedCmd
+        | RunningTrue -> {state with running = true}, Cmd.none
+        | RunningFalse -> {state with running = false}, Cmd.none
     
-    let view (state: State) (dispatch) =
+    let view (state: State) (dispatch: Msg -> unit) =
         DockPanel.create [
             DockPanel.children [
+                Button.create [
+                    Button.dock Dock.Bottom
+                    Button.onClick (fun _ -> dispatch IncrementDelayed)
+                    Button.content "incrementDelayed"
+                ]                
+                Button.create [
+                    Button.dock Dock.Bottom
+                    Button.onClick (fun _ -> dispatch RunningTrue)
+                    Button.content "runningTrue"
+                ]                
+                Button.create [
+                    Button.dock Dock.Bottom
+                    Button.onClick (fun _ -> dispatch RunningFalse)
+                    Button.content "runningFalse"
+                ]                
                 Button.create [
                     Button.dock Dock.Bottom
                     Button.onClick (fun _ -> dispatch Reset)
@@ -33,6 +75,13 @@ module Counter =
                     Button.dock Dock.Bottom
                     Button.onClick (fun _ -> dispatch Increment)
                     Button.content "+"
+                ]
+                TextBlock.create [
+                    TextBlock.dock Dock.Top
+                    TextBlock.fontSize 48.0
+                    TextBlock.verticalAlignment VerticalAlignment.Center
+                    TextBlock.horizontalAlignment HorizontalAlignment.Center
+                    TextBlock.text (string state.running)
                 ]
                 TextBlock.create [
                     TextBlock.dock Dock.Top
--- Main/Program.fs
+++ Main/Program.fs
@@ -1,5 +1,7 @@
 ﻿namespace Main
 
+open System
+open Avalonia.Threading
 open Elmish
 open Avalonia
 open Avalonia.Controls.ApplicationLifetimes
@@ -7,6 +9,7 @@ open Avalonia.Input
 open Avalonia.FuncUI
 open Avalonia.FuncUI.Elmish
 open Avalonia.FuncUI.Components.Hosts
+open Main.Counter
 
 type MainWindow() as this =
     inherit HostWindow()
@@ -15,12 +18,20 @@ type MainWindow() as this =
         base.Width <- 400.0
         base.Height <- 400.0
         
-        //this.VisualRoot.VisualRoot.Renderer.DrawFps <- true
-        //this.VisualRoot.VisualRoot.Renderer.DrawDirtyRects <- true
+        let timer (_state: Counter.State) =
+          let sub (dispatch: Counter.Msg -> unit) =
+            let invoke() =
+              Counter.IncrementIfRunning |> dispatch
+              true
+            DispatcherTimer.Run(Func<bool>(invoke), TimeSpan.FromMilliseconds 1000.) |> ignore
+          Cmd.ofSub sub
+        this.VisualRoot.VisualRoot.Renderer.DrawFps <- true
+        this.VisualRoot.VisualRoot.Renderer.DrawDirtyRects <- true
 
 
-        Elmish.Program.mkSimple (fun () -> Counter.init) Counter.update Counter.view
+        Elmish.Program.mkProgram (fun () -> Counter.init()) Counter.update Counter.view
         |> Program.withHost this
+        |> Program.withSubscription timer
         |> Program.run
 
         
