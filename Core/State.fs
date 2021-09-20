8bd482605574751be81ef3c3240a1be57a8e284d
skogix <skogix@gmail.com>
2021-09-20 16:29:45 +0200

    pre uppdatera debug via main

---
+++ Core/State.fs
@@ -0,0 +1,22 @@
+module Core.State
+
+open Core.Debug.State
+
+type ExampleOutput = {
+  running: bool
+  count: int
+}
+type TestOutput = {
+  stringValue: string
+  intValue: int
+  debugOutput: Core.Debug.State.DebugData list
+}
+type DebugManager = {
+  manager: Debug.State.DebugManager
+  messages: Debug.State.DebugMessage list
+}
+type ShellState = {
+  Test: TestOutput
+  Example: ExampleOutput
+  Debug: DebugManager
+}
