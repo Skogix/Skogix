dcb20a7acc5fca836f3c99776daa5a7259a52f21
skogix <skogix@gmail.com>
2021-09-20 15:08:16 +0200

    jävla directories i f#

---
+++ Core/Debug/State.fs
@@ -0,0 +1,41 @@
+module Core.Debug.State
+
+open System
+open System.Collections.Generic
+
+type DebugMessage = string
+type DebugData = {
+  Message: DebugMessage
+  Time: DateTime
+}
+type DebugMailboxMessage =
+  | Send of string
+  | Get of AsyncReplyChannel<DebugData list>
+type DebugManager() =
+  let mailbox = MailboxProcessor.Start(fun inbox ->
+    let rec loop (currentData: DebugData list) = async {
+      let! mail = inbox.Receive()
+      match mail with
+      | Send debugMessage ->
+        let debugData = {
+          Message = debugMessage
+          Time = DateTime.Now }
+        return! loop (debugData :: currentData)
+      | Get (rc) ->
+        rc.Reply currentData
+        return! loop []
+    }
+    loop []
+    )
+  member this.Send message = mailbox.Post (Send message)
+  member this.Get = mailbox.PostAndAsyncReply (Get)
+let debugManager = DebugManager()
+
+for i in [1..10] do
+  debugManager.Send (sprintf "Item %i" i)
+
+let asyncData = debugManager.Get
+asyncData |> Async.RunSynchronously |> List.iter (printfn "%A")
+
+let asyncData2 = debugManager.Get
+asyncData2 |> Async.RunSynchronously |> List.iter (printfn "%A")
